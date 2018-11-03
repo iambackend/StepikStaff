108354a19226d5204790a955ae788eaa58ca064e
iambackend <iambackend@gmail.com>
2018-11-03 17:04:35 +0300

    .py module specified

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080
+gunicorn -b 0.0.0.0:8080 hello.py:app
