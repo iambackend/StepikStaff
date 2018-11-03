bdeae2acdbce043e828b6e288244c8ca833aed78
iambackend <iambackend@gmail.com>
2018-11-03 22:47:28 +0300

    F

--- hello.py
+++ hello.py
@@ -6,6 +6,9 @@ def app(environ, start_response):
         ('Content-type', 'text/plain')
     ]
     start_response(status, response_headers)
+    f1=open('/home/box/web/outttttttt.txt', 'w+')
+	f1.write(environ)
+	f1.close()
     print(environ['QUERY_STRING'])
     return ['\r\n'.join(environ['QUERY_STRING'].split('&'))]
     # return iter([data])
