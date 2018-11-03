738547260a97df9512da0bf5f13ee88885e8933e
iambackend <iambackend@gmail.com>
2018-11-03 19:53:14 +0300

    hello data 'fix'

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+curl 0.0.0.0:8080/?a=1&b=34&c=1488
+curl 127.0.0.1:80/hello/?a=1&b=34&c=1488
 # gunicorn -b 0.0.0.0:8080 hello.py:app
