60749dd69f4cee9731e097a624d81150140d7ebf
iambackend <iambackend@gmail.com>
2018-11-03 23:44:54 +0300

    F

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-# gunicorn -b 0.0.0.0:8080 hello.py:app
