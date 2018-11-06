13d8c3e455f311f89fab453e122dc88eb24f93b2
iambackend <iambackend@gmail.com>
2018-11-06 20:14:46 +0300

    gunicorn settings fix

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test_hello
+sudo ln -s /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/test_django
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf
 # gunicorn -b 0.0.0.0:8080 hello.py:app
