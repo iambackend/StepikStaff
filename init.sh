2b82b5cbf2404ceae9f2fec780c68f855a1235d1
iambackend <iambackend@gmail.com>
2018-11-06 19:44:59 +0300

    django addition 0.1

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf
 # gunicorn -b 0.0.0.0:8080 hello.py:app
