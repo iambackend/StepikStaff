ec7ebde19d0ea2fc3f1f3069a07a2f9449cc5d2b
iambackend <iambackend@gmail.com>
2018-11-10 13:27:27 +0300

    F

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 curl "0.0.0.0:8080/?a=1&b=34&c=1488"
 echo ;
 curl "127.0.0.1/hello/?a=1&b=34&c=1488"
+echo ;
+curl "0.0.0.0:8080/popular"
 curl "127.0.0.1/"
 curl "127.0.0.1/login/"
 curl "127.0.0.1/question/123/"
